1e1fff40a73f98491cbc563910f32922c662ef79
darrelhong <darrel.hong@gmail.com>
2023-10-11 09:08:07 +0800

    add LWWRegister

---
+++ LWWRegister.ts
@@ -0,0 +1,32 @@
+export class LWWRegister<T> {
+  readonly id: string;
+  state: [peerId: string, timestamp: number, value: T];
+
+  constructor(id: string, val: T) {
+    this.id = id;
+    this.state = [id, 0, val];
+  }
+
+  get(): T {
+    return this.state[2];
+  }
+
+  set(value: T): void {
+    this.state = [this.id, this.state[1] + 1, value];
+  }
+
+  merge(state: [peerId: string, timestamp: number, value: T]): void {
+    const [remotePeerId, remoteTimestamp] = state;
+    const [localPeerId, localTimestamp] = this.state;
+
+    if (localTimestamp > remoteTimestamp) {
+      return;
+    }
+
+    if (localTimestamp == remoteTimestamp && localPeerId > remotePeerId) {
+      return;
+    }
+
+    this.state = state;
+  }
+}
