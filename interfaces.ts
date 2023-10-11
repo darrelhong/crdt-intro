c339b436c480cd2d408d616bce7f1e2d9f3066a4
darrelhong <darrel.hong@gmail.com>
2023-10-11 09:09:22 +0800

    add map interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,5 +1,15 @@
+import { LWWRegister } from "./LWWRegister";
+
 interface CRDT<T, S> {
   value: T;
   state: S;
   merge(state: S): void;
 }
+
+type Value<T> = {
+  [key: string]: T;
+};
+
+type State<T> = {
+  [key: string]: LWWRegister<T | null>["state"];
+};
